259d5eb6717c3d76b1d4f23ca88c089a0682ec57
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-05-21 17:32:29 +0900

    add favicon & title

--- page.py
+++ page.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
+st.set_page_config(page_title='McDonald EDA', page_icon = "https://img.icons8.com/color/452/mcdonalds.png")#, layout = 'wide', initial_sidebar_state = 'auto')
+
 st.write("HELLO HI HELLO")
