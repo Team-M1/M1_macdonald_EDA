676e01c9d234d0f985a8153b399010423c24fb23
mjh0528 <mjd0528@gmail.com>
2021-05-23 20:09:49 +0900

    mod get plot from static img from page.py

--- page.py
+++ page.py
@@ -14,28 +14,13 @@ class McDonald:
     def head(self) -> pd.DataFrame:
         return self.data.head(5)
 
-    def CPF_and_calories_relationship_graph(self):
+    def CPF_and_calories_relationship(self):
         fig, ax = plt.subplots(1, 3, sharey=True, figsize=(15, 5))
         x = ["Carbohydrates", "Protein", "Total Fat"]
         for i in range(3):
             sns.regplot(x=x[i], y="Calories", data=self.data, ax=ax[i])
         st.pyplot(fig)
-
-    def weight_and_calories_relationship_graph(self):
-        size_calories_data = self.data[["Serving Size", "Calories", "Item"]]
-        idx = (
-            size_calories_data["Serving Size"].str.contains("cup").map(lambda x: not x)
-        )
-        size_calories_data = size_calories_data.loc[idx, :]
-        size_calories_data["gram"] = (
-            size_calories_data["Serving Size"].apply(lambda x: x.split("(")[-1][:3]).astype(float)
-        )
-        dfX = size_calories_data["gram"]
-        dfy = size_calories_data["Calories"]
-
-        fig, ax = plt.subplots()
-        sns.regplot(x=dfX, y=dfy)
-        st.pyplot(fig)
+        #plt.savefig('CPF_and_calories_relationship.png')
     
     def all_categories(self):
         new_data = self.data.groupby('Category').mean()
@@ -44,11 +29,13 @@ class McDonald:
         for i in range(len(ax)):
             sns.barplot(x=new_data.index, y=new_data.columns[i], data=new_data, ax=ax[i])
         st.pyplot(fig)
+        #plt.savefig('all_categories.png')
     
     def fat_relationship_sum(self):
         fig, ax = plt.subplots(1, 1, figsize=(8,8))
         sns.scatterplot(x='Saturated Fat', y='Total Fat', hue='Trans Fat', s=100, data=self.data)
         st.pyplot(fig)
+        #plt.savefig('fat_relationship_sum.png')
     
     def fat_relationship_sep(self):
         fig = plt.figure(figsize=(16,8))
@@ -66,11 +53,16 @@ class McDonald:
         ax2.set_xlabel('Trans Fat')
         ax2.set_ylabel('Total Fat')
         st.pyplot(fig)
+        #plt.savefig('fat_relationship_sep.png')
     
     def heatmap(self):
         fig, ax = plt.subplots(1, 1)
         sns.heatmap(self.data.corr())
-        st.pyplot(fig)        
+        st.pyplot(fig)
+        #plt.savefig('heatmap.png')
+
+
+mcdonald = McDonald()
 
 
 def sidebar():
@@ -85,29 +77,27 @@ def sidebar():
 
 
 def main():
-    mcdonald = McDonald()
-
     st.markdown("# Mcdonald EDA")
     st.markdown("맥도날드 데이터셋을 사용하여 분석한 EDA 프로젝트 코드입니다.")
 
     st.markdown("## head 로 빠르게 살펴보기")
-    st.dataframe(mcdonald.head())
+    mcdonald.head()
 
     st.markdown("## 히트맵")
-    mcdonald.heatmap()
+    st.image('heatmap.png')
 
     st.markdown("## 카테고리별 비교")
-    mcdonald.all_categories()
+    st.image('all_categories.png')
     st.markdown("`Smoothies & Shakes` 메뉴의 탄수화물, 당류가 높게 나타났다. (당류의 경우 압도적으로 높게 나타났다.)")
     st.markdown("`Breakfast` 메뉴의 콜레스테롤이 높게 나타났다.")
     st.markdown("`Beef & Pork`, `Breakfast`, `Chicken & Fish` 메뉴들은 대체로 영양성분이 비슷했다.")
 
     st.markdown("## 칼로리와 탄단지의 전반적인 상관관계")
-    mcdonald.CPF_and_calories_relationship_graph()
+    st.image('CPF_and_calories_relationship.png')
 
     st.markdown("## Total Fat은 Saturated Fat 과 Trans Fat과 관련성이 있는가?\n- 가정 : Saturated Fat 과 Trans Fat 모두 지방이므로 전체 지방 미치는 영향이 비슷할 것이다.")
-    mcdonald.fat_relationship_sum()
-    mcdonald.fat_relationship_sep()
+    st.image('fat_relationship_sep.png')
+    st.image('fat_relationship_sum.png')
     st.markdown("Total Fat은 Saturated Fat과 관계도는 0.847 정도로 관계성이 높으나, Trans Fat 과의 관계성은 0.434 정도로 생각보다 낮은 경향을 보였다.")
 
 if __name__ == "__main__":
